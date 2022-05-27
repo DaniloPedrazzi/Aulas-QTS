918b32fb295243b6eac113dd58372b501e559896
Danilo <74073936+xCUELHO@users.noreply.github.com>
2022-05-27 08:33:07 -0300

    Add files via upload

---
+++ assert.test.js
@@ -0,0 +1,20 @@
+import {
+    assertFalse,
+    assertExists,
+    assertNotStrictEquals,
+} from "http://deno.land/std/testing/asserts.ts"
+
+let mentira = false;
+let existe = "qualquer coisa";
+
+Deno.test("Detector de mentira", () => {
+    assertFalse(mentira); 
+})
+
+Deno.test("Ser ou não ser", ()=> {
+    assertExists(existe);
+})
+
+Deno.test(), () => {
+    
+}
